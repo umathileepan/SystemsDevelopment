ebf8803051d75e93c9c5e4f3a68e85de64257288
Uma Thileepan <77700057+umathileepan@users.noreply.github.com>
2026-04-05 10:35:53 +0100

    Dog Class
    The Dog class has two constructors and a method called Description.

--- Program.cs
+++ Program.cs
@@ -1,90 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-
-var todos = new List<string>();
-
-Console.WriteLine("Hello!\n\n");
-do {
-    Console.WriteLine("What do you want to do?");
-    Console.WriteLine("[S]ee all todos");
-    Console.WriteLine("[A]dd a todo");
-    Console.WriteLine("[R]emove a todo");
-    Console.WriteLine("[E]xit");
-    var answer = Console.ReadLine();
-    switch (answer)
+﻿internal class Program
+{
+    private static void Main(string[] args)
     {
-        case "S":
-        case "s":
-            if(todos.Count == 0)
-            {
-                Console.WriteLine("No TODOs have been added yet.\n\n");
-                break;
-            }
-            for (int i = 0; i < todos.Count; i++)
-            {
-                Console.WriteLine(value: $"{i+1}. {todos[i]}");
-            }
-            
-            Console.WriteLine("\n");
-            break;
-        case "A":
-        case "a":
-            Console.WriteLine("Enter the TODO description:");
-            var todo = Console.ReadLine();
-            if (todo == null || todo.Trim() == "")
-            {
-                Console.WriteLine("The description cannot be empty.");
-                break;
-            }
-            if (todos.Contains(todo))
-            {
-                Console.WriteLine("The description must be unique.");
-                break;
-            }
-            todos.Add(todo);
-            Console.WriteLine($"TODO successfully added: {todo}\n\n");
-            break;
-        case "R":
-        case "r":
-            Console.WriteLine("Select the index of the TODO you want to remove");
-            var todoToRemove = Console.ReadLine();
-            if (todoToRemove == null || todoToRemove.Trim() == "")
-            {
-                Console.WriteLine("Selected index cannot be empty.");
-                break;
-            }
-            /*else if (!todos.Contains(todoToRemove))
-            {
-                Console.WriteLine("Todo not found.");
-               
-            }*/
-            else if (!int.TryParse(todoToRemove, out int index) || index < 1 || index > todos.Count)
-            {
-                Console.WriteLine("Todo not found");
-            }
-            else
-            {
-                if(todos.Count == 0)
-                {
-                    Console.WriteLine("List is empty");
-                }
-                else
-                {
-                    int indexToRemove = int.Parse(todoToRemove) - 1;
-                    todos.RemoveAt(indexToRemove);
-                    Console.WriteLine($"TODO Removed: {todos[indexToRemove]}");
-                }
-              
-            }
-            break;  
-        case "E":
-        case "e":
-            Console.WriteLine("Goodbye!");
-            return;
-        default:
-            Console.WriteLine("Invalid choice.");
-            break;
-    }
-} while (true);
+        Dog d1 = new Dog("Lucky", "German Shepherd", 40);
+        Dog d2 = new Dog("Tina", "Shar pei", 25);
+        Dog d3 = new Dog("Rex", 10);
+        Console.WriteLine(d1.Describe());
+        Console.WriteLine(d2.Describe());
+        Console.WriteLine(d3.Describe());
+        Console.ReadKey();
 
-Console.ReadKey();
+    }
+}
+
+public class Dog
+{
+    private string _name;
+    private string _breed;
+    private int _weight;
+
+    public Dog(string name, string breed, int weight)
+    {
+        _name = name;
+        _breed = breed;
+        _weight = weight;
+    }
+
+    public Dog(string name, int weight) : this(name, "mixed-breed", weight)
+    {
+        
+    }
+
+    public string Describe() 
+    {
+        string description;
+        if (_weight < 5)
+        {
+            description = "tiny";
+        }
+        else if (_weight >= 5 && _weight < 30)
+        {
+            description = "medium";
+        }
+        else
+        {
+            description = "large";
+        }
+        return $"This dog is named {_name}, it's a {_breed}, and it weighs {_weight} kilograms, so it's a {description} dog.";
+    }
+}
